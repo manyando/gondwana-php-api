11f9ee1b48f5486e5bb64c3e252081588953380b
Manyando Kaine <mkaine@mtc.com.na>
2025-06-11 11:32:28 +0200

    updated index.php

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-require 'helperFunctions.php';
+use HelperFunctions\HelperFunctions;
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(204);
