1f40e5c86507e58ad509ec908fec844056e75c27
Manyando Kaine <mkaine@mtc.com.na>
2025-06-12 16:18:25 +0200

    updated access control

--- public/api/index.php
+++ public/api/index.php
@@ -1,7 +1,6 @@
 <?php
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 require_once './utilities/helperFunctions.php';
